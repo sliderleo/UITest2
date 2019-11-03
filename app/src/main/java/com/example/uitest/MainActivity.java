58aab153096ce39286379a5926da0a5f580ca239
Daniel Leo <36951736+sliderleo@users.noreply.github.com>
2019-11-03 21:27:09 +0800

    User UI and Vehicle UI update

--- app/src/main/java/com/example/uitest/MainActivity.java
+++ app/src/main/java/com/example/uitest/MainActivity.java
@@ -8,18 +8,20 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private CardView prof,vehicle;
+    private CardView prof,vehicle,tow,location;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         prof =findViewById(R.id.profCard);
         vehicle= findViewById(R.id.vehicleCard);
-
-
+        tow=findViewById(R.id.towCard);
+        location=findViewById(R.id.locationCard);
 
         prof.setOnClickListener(this);
         vehicle.setOnClickListener(this);
+        tow.setOnClickListener(this);
+        location.setOnClickListener(this);
     }
 
         @Override
@@ -28,7 +30,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             switch(v.getId()) {
                 case R.id.profCard: i = new Intent(this, User.class);startActivity(i);break;
-                case R.id.vehicleCard: i = new Intent(this, Location.class);startActivity(i);break;
+                case R.id.vehicleCard: i = new Intent(this, Vehicle.class);startActivity(i);break;
+                case R.id.towCard: i = new Intent(this, Login.class);startActivity(i);break;
+                case R.id.locationCard: i = new Intent(this, Location.class);startActivity(i);break;
             }
         }
     }
