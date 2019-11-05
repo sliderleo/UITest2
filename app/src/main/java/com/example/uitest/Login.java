08e9234d1f5e07259fd4bc148b03359adde8ce78
Daniel Leo <36951736+sliderleo@users.noreply.github.com>
2019-11-05 14:59:35 +0800

    Login

--- app/src/main/java/com/example/uitest/Login.java
+++ app/src/main/java/com/example/uitest/Login.java
@@ -2,18 +2,20 @@ package com.example.uitest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class Login extends AppCompatActivity implements View.OnClickListener {
+public class Login extends AppCompatActivity{
     private Button registerBtn,loginBtn;
     private EditText etUsername,etPassword;
     String username,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        EditText UsernameET,PasswordET;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         //buttons
@@ -23,21 +25,16 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         etUsername=(EditText)findViewById(R.id.login_username);
         etPassword=(EditText)findViewById(R.id.login_password);
         //onClickListener
-        registerBtn.setOnClickListener(this);
+        //registerBtn.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View v) {
-        Intent i;
 
-        switch (v.getId()){
-            case R.id.register_button: i = new Intent(this,Register.class);startActivity(i);break; // redirect to register activity
-            case R.id.login_button: login(); break;
-        }
+    public void onLogin(View v) {
+        String username= etUsername.getText().toString();
+        String password= etPassword.getText().toString();
+        String type = "login";
+        LoginBackground backgroundWorker = new LoginBackground(this);
+        backgroundWorker.execute(type,username,password);
     }
 
-    public void login(){ //login function
-        username=etUsername.getText().toString();
-        password=etPassword.getText().toString();
-    }
 }
